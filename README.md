7622d970b0b34b950c5e5c1fc13b9e93acde3d1e
Arezoo Mohammadi <arezoomohammadi493@gmail.com>
2026-04-28 08:50:41 +0200

    Aufgabe erledigt

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # API Project lolo
  ## Git Setup
 
- git config --gobal user.name
+ git config --gobal user.name "Arezoo Mohammadi"
  git config --gobal user.email
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/name/{name}")
+def greet_name(name: str):
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/add/{number}")
+def add_number(number: int):
+    result = number + 1
+    return {
+        "input": number,
+        "operation": "add 1",
+        "result": result
+    }
+
+
+@app.get("/double/{number}")
+def double_number(number: int):
+    result = number * 2
+    return {
+        "input": number,
+        "operation": "double",
+        "result": result
+    }
